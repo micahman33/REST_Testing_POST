d70819e5b058d0bbc948a3be90948250bf314b17
Micah Smith <micahdanielsmith@gmail.com>
2021-02-10 08:55:42 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -27,7 +27,7 @@ public class Main {
         String url = "https://ap17.salesforce.com/services/data/v49.0/sobjects/Case";
         String line;
         StringBuffer responseContent = new StringBuffer();
-        String token = "00D6g000006MyrF!ASAAQNhhySnPKrosZAb983aITbyIcNMUMQsomcq6K1DgE4LC5V7hRDHMUZouTzOVkLwLFZ0APsIgi5n1B4.wcd3Qxj_Zksfr";
+        String token = "00D6g000006MyrF!ASAAQNhhySnPKrVkLwLFZ0APsIgi5n1B4.wcd3Qxj_Zksfr";
         String jsonBody = "{\"OwnerId\": \"0056g000003eaZOAAY\",\n" +
                 "       \"Subject\": \"Delete\",\n" +
                 "       \"Description\": \"PO Tracker Document Status Notification: The following document(s) have been identified as being associated to notification name: New POs are available on Automation Anywhere Test PO Tracker.\\r\\n\\r\\nDateSender (EID)Receiver (EID)PO#PO Line #Control NumberBusiness ProcessFlow ID\\r\\nJuly 13, 2020 9:01:57 AM Deckow Inc 5548 (10553474) Automation Anywhere, INC. (3937663) 44922241 34-840-0560 Purchase Order d6fhghh3gf2addfdfdsd7hdf29dj4gh3h\\r\\n\\r\\nContact Automation Anywhere Test PO Tracker team if you have any questions regarding this or other issues. Customer Care analysts are available at 1-888-555-5555\"}";
